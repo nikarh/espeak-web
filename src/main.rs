c7c68ca27d854acd800e39524e966200bf0bff82
Nikolay Arhipov <n@arhipov.net>
2024-01-09 12:57:21 +0200

    rouille -> tiny_http

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::{env, net::Ipv4Addr, process, sync::Mutex};
 
-use rouille::{try_or_400, Response};
-
 fn main() {
     let port = env::var("PORT")
         .ok()
@@ -11,22 +9,33 @@ fn main() {
     eprintln!("Starting server on port {port}");
 
     let guard = Mutex::<()>::default();
-    rouille::start_server((Ipv4Addr::UNSPECIFIED, port), {
-        move |request| {
-            if request.method() != "POST" {
-                return Response::empty_404();
-            }
 
-            let body = try_or_400!(rouille::input::plain_text_body(request));
-            eprintln!("Speaking `{body}`");
+    let Ok(server) = tiny_http::Server::http((Ipv4Addr::UNSPECIFIED, port)) else {
+        eprintln!("Failed to start server");
+        return;
+    };
 
-            // Espeak can not parallelize
-            let _lock = guard.lock().unwrap();
-            if let Err(err) = process::Command::new("espeak").arg(body).output() {
-                eprintln!("Espeak failed: {err}");
-            }
-
-            Response::empty_204()
+    for mut request in server.incoming_requests() {
+        if request.method() != &tiny_http::Method::Post {
+            let _ = request.respond(tiny_http::Response::empty(404));
+            continue;
         }
-    });
+
+        let mut body = String::new();
+        if let Err(err) = request.as_reader().read_to_string(&mut body) {
+            eprintln!("Failed to read request body: {err}");
+            let _ = request.respond(tiny_http::Response::empty(400));
+            continue;
+        }
+
+        eprintln!("Speaking `{body}`");
+
+        // Espeak can not parallelize
+        let _lock = guard.lock().unwrap();
+        if let Err(err) = process::Command::new("espeak").arg(body).output() {
+            eprintln!("Espeak failed: {err}");
+        }
+
+        let _ = request.respond(tiny_http::Response::empty(204));
+    }
 }
